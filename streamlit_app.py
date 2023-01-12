a983b0e59896d3560806c834e0eb01bac95436be
souravg6996 <122539931+souravg6996@users.noreply.github.com>
2023-01-12 09:44:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('My family new healthy diner')
+streamlit.header('Breakfast menu')
+streamlit.text(':bowl_with_spoon: Omega 3 & Blueberries Oatmeal')
+streamlit.text(':green_salad: Kale, Spinach & Rocket Smoothie')
+streamlit.text(':chicken: Hard-Boiled Free-range Egg')
 
